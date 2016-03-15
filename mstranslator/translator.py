48c213760f55069614c1c65ea958d369b5138b70
Sahil Gupta <sahilgupta.29@gmail.com>
2016-03-15 22:16:00 +0530

    Style fixes

--- mstranslator/translator.py
+++ mstranslator/translator.py
@@ -1,9 +1,13 @@
-import requests
+"""Python wrapper for Microsoft Translate services."""
+
 import urllib
 import sys
+import requests
 
-class Config:
+
+class Config(object):
     """Config to be given to an instance of translator to do the Authorization."""
+
     def __init__(self, translator_client_id, translator_client_secret):
         assert translator_client_id is not None
         assert type(translator_client_id) is str
@@ -14,12 +18,14 @@ class Config:
         self.translator_client_id = translator_client_id
         self.translator_client_secret = translator_client_secret
 
-class Translator:
+
+class Translator(object):
     """An instance of this class can be used to detect language and translate text."""
+
     def __init__(self, config):
         self.config = config
 
-    def __get_access_token(self):
+    def _get_access_token(self):
         data = {"client_id": self.config.translator_client_id,
          "client_secret": self.config.translator_client_secret,
          "scope": 'http://api.microsofttranslator.com',
@@ -27,42 +33,49 @@ class Translator:
         resp = requests.post(url='https://datamarket.accesscontrol.windows.net/v2/OAuth2-13', data=urllib.urlencode(data))
         return resp.json()["access_token"]
 
-    def __authorization_header(self):
-        access_token = self.__get_access_token()
+    def _authorization_header(self):
+        access_token = self._get_access_token()
         return "Bearer" + " " + access_token
 
     def detect_language(self, text):
         text = text.encode('utf-8')
-        authorization_header = self.__authorization_header()
+        authorization_header = self._authorization_header()
         headers = {"Authorization": authorization_header}
         data = {"text": text}
-        resp = requests.get(url='http://api.microsofttranslator.com/v2/Http.svc/Detect', params=data, headers=headers)
-        try:
-            t = resp.text.encode('utf-8')
-            # Different unicodes for different languages are not parsed correctly
-            # with xml module.
-            detected_language_code = t.split('>')[1].split('<')[0]
-            return (detected_language_code, authorization_header)
-        except Exception as e:
-            sys.stderr.write(e)
-            raise
+        resp = requests.get(url='http://api.microsofttranslator.com/v2/Http.svc/Detect',
+                            params=data, headers=headers)
+        if resp.ok:
+            try:
+                t = resp.text.encode('utf-8')
+                # Different unicodes for different languages are not parsed
+                # correctly with xml module.
+                detected_language_code = t.split('>')[1].split('<')[0]
+                return detected_language_code
+            except Exception as e:
+                sys.stderr.write(e)
+                raise
+        else:
+            raise resp.raise_for_status()
 
-    def translate(self, text, from_language, to_language, authorization_header):
-        if authorization_header is None:
-            authorization_header = self.__authorization_header()
+    def translate(self, text, from_language, to_language):
         text = text.encode('utf-8')
+        authorization_header = self._authorization_header()
         headers = {"Authorization": authorization_header}
-        data = {"text": text,
-         "from": from_language,
-         "to": to_language}
-        resp = requests.get(url='http://api.microsofttranslator.com/v2/Http.svc/Translate', params=data, headers=headers)
-        try:
-            t = resp.text.encode('utf-8')
-            translatedText = t.split('>')[1].split('<')[0]
-            return translatedText
-        except Exception as e:
-            sys.stderr.write("Could not parse XML {0}".format(resp.text))
-            raise
+        data = {
+            "text": text,
+            "from": from_language,
+            "to": to_language
+        }
+        resp = requests.get(url='http://api.microsofttranslator.com/v2/Http.svc/Translate',
+                            params=data, headers=headers)
 
-if __name__ == '__main__':
-    print translate(sys.argv[1], sys.argv[2], sys.argv[3])
+        if resp.ok:
+            try:
+                t = resp.text.encode('utf-8')
+                translated_text = t.split('>')[1].split('<')[0]
+                return translated_text
+            except Exception:
+                sys.stderr.write(u"Could not parse XML {0}".format(resp.text))
+                raise
+        else:
+            raise resp.raise_for_status()
