8e8b804d4c9a98c8b593b55aacd12617f258779f
Ayush Goel <ayushgoel111@gmail.com>
2016-03-19 00:14:07 +0530

    Update version to 1.0.0

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from distutils.core import setup
 setup(
     name = 'mstranslator-2016',
     packages = ['mstranslator'],
-    version = '0.1.0',
+    version = '1.0.0',
     description = 'Python wrapper to consume Microsoft translator API',
     author = 'Ayush Goel',
     author_email = 'ayushgoel111@gmail.com',
